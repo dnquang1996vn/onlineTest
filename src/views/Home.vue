34c558c0bd29704274439c77510eb62940d0d151
Do Nhat Quang <quang.dn@altplus.com.vn>
2019-07-11 11:27:42 +0700

    change welcome text

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <h1>Welcome to project</h1>
+  <div class="welcome-text">
+    Welcome to project
   </div>
 </template>
 
@@ -10,3 +10,10 @@ export default {
   name: "home"
 };
 </script>
+<style>
+.welcome-text {
+  font-size: 8rem;
+  text-align: center;
+  margin: 15%;
+}
+</style>
